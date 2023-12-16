b121ab203871de4b534d0f8ed0aa36f5ecb19016
Trang Vu <106600297+Trangvu801@users.noreply.github.com>
2023-12-16 18:55:33 -0500

    Cypress test for table

--- farmdata2/farmdata2_modules/fd2_school/fd2/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/e2e.defaults.spec.js
@@ -1,28 +1,11 @@
-describe("Test the harvest report default values", () => {
+describe("Test contents of harvest report table", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-school/fd2")
     })
 
-    it("Check the page header", () => {
-        cy.get("[data-cy=page-header]")
-            .should("have.text","Harvest Report")
-    })
-
-    it("Check start and end dates", () => {
-        cy.get("[data-cy=start-date]")
-            .should("have.value","2020-05-05")
-        cy.get("[data-cy=end-date]")
-            .should("have.value","2020-05-15")
-    })
-
-    it("Check crop value", () => {
-        cy.get("[data-cy=select-crop] > [data-cy=dropdown-input] > [data-cy=option0]")
-        .should("have.value","All")
-        cy.get("[data-cy=select-crop] > [data-cy=dropdown-input] > [data-cy=option4]")
-        .should("have.value","BEAN-DRY")
-        cy.get("[data-cy=select-crop]> [data-cy=dropdown-input]").children()
-        .should("have.length",112)
+    it("", () => {
 
     })
+
 })
