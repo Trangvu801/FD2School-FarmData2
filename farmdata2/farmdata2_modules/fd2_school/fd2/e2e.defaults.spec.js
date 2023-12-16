7cbb814fddca6d5f8eec54c34cb354e6486b1dc6
Trang Vu <106600297+Trangvu801@users.noreply.github.com>
2023-12-16 18:59:03 -0500

    Add check number of columns

--- farmdata2/farmdata2_modules/fd2_school/fd2/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/e2e.defaults.spec.js
@@ -11,6 +11,8 @@ describe("Test contents of harvest report table", () => {
         cy.get("[data-cy=h2]").should("have.text","Crop")
         cy.get("[data-cy=h3]").should("have.text","Yield")
         cy.get("[data-cy=h4]").should("have.text","Units")
+        cy.get("[data-cy=table-headers]").children()
+        .should("have.length",5)
     })
 
 })
