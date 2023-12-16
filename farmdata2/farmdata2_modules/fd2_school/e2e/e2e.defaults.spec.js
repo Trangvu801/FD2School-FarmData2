6ec600ddaed083d52755f96e5f6a6db6620314c5
Trang Vu <106600297+Trangvu801@users.noreply.github.com>
2023-12-16 16:37:00 -0500

    Added test for crop dropdown

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -15,4 +15,16 @@ describe("Test the harvest report default values", () => {
         cy.get("[data-cy=end-date]")
             .should("have.value","2020-05-15")
     })
+
+    it("Check crop value", () => {
+        cy.get("[data-cy=select-crop]").children()
+            .should("have.length",111)
+        cy.get("[data-cy=select-crop]").children().eq(0)
+            .should("have.value","ASPARAGUS")
+        cy.get("[data-cy=select-crop]").children().eq(4)
+            .should("have.value","BEAN-FAVA")
+        cy.get("[data-cy=select-crop]").children().eq(110)
+            .should("have.value","ZUCCHINI")
+
+    })
 })
