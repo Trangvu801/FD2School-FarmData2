8da76cdbcd2cc86f0c7d34c0294d8494f01c8478
Trang Vu <106600297+Trangvu801@users.noreply.github.com>
2023-12-16 18:57:04 -0500

    Test for report table headers

--- farmdata2/farmdata2_modules/fd2_school/fd2/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/e2e.defaults.spec.js
@@ -4,8 +4,13 @@ describe("Test contents of harvest report table", () => {
         cy.visit("/farm/fd2-school/fd2")
     })
 
-    it("", () => {
-
+    it("Check table headers", () => {
+        cy.get("[data-cy=generate-button]").click()
+        cy.get("[data-cy=h0]").should("have.text","Date")
+        cy.get("[data-cy=h1]").should("have.text","Area")
+        cy.get("[data-cy=h2]").should("have.text","Crop")
+        cy.get("[data-cy=h3]").should("have.text","Yield")
+        cy.get("[data-cy=h4]").should("have.text","Units")
     })
 
 })
